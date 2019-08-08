f940b74de911a9f5923b93675262eea8c1f666a1
mgk <mgk.021@gmail.com>
2019-08-08 23:48:05 +0430

    Updates

---
+++ precache-manifest.5d823c0763fd7cb23210e526907562ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7bf6fef09278b3ac69079144463dfc",
+    "url": "/react-list-app/index.html"
+  },
+  {
+    "revision": "55e700f8bb6064111b78",
+    "url": "/react-list-app/static/css/main.f79199e3.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/react-list-app/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "55e700f8bb6064111b78",
+    "url": "/react-list-app/static/js/main.10c24677.chunk.js"
+  },
+  {
+    "revision": "47b66580e20f7a2ce051",
+    "url": "/react-list-app/static/js/runtime~main.74ef6b00.js"
+  }
+]);
